f837a0375e7e4a83327f47d5abc9e1a1d425eab2
chakjc65 <chak.jagannathan@gmail.com>
2018-06-03 15:35:03 -0400

    Test github

--- insert.sql
+++ insert.sql
@@ -1,3 +1,4 @@
+--- comments
 insert into todoentity (id,name,status) values (1001,'my first todo item-1',0);
 insert into todoentity (id,name,status) values (1002,'my 2nd todo item-2',1);
 insert into todoentity (id,name,status) values (1003,'my 3rd todo item-3',1);
